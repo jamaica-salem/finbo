f64e83a065096a459f543fe5c810fe9df5386a78
Jamaica Salem <jamaica.esalem@gmail.com>
2026-04-01 19:59:16 +0800

    style: replace Space Grotesk font with Satoshi for headings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        heading: ["Space Grotesk", "sans-serif"],
+        heading: ["Satoshi", "sans-serif"],
         body: ["Inter", "sans-serif"],
       },
       colors: {
