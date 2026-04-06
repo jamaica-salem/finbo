bd77fb50cb5617a99c4e56efe23e355a6e6a4280
Jamaica Salem <jamaica.esalem@gmail.com>
2026-04-06 14:31:23 +0800

    fix(theme): apply stored theme on startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme === 'dark') {
+  document.documentElement.classList.add('dark');
+} else if (storedTheme === 'light') {
+  document.documentElement.classList.remove('dark');
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
